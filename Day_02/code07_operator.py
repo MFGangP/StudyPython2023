b4127aff0074bd55ad21b64ede2aa3faa8b4e5c5
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-01-31 17:08:19 +0900

    2일차 교육 정리
    2023_01_31 교육 종료

--- Day_02/code07_operator.py
+++ Day_02/code07_operator.py
@@ -106,7 +106,6 @@ que.remove(10)
 print(que)
 # remove는 원하는 값 삭제
 
-
 # 튜플은 기능 자체가 없음
 # TypeError: 'tuple' object does not support item assignment
 tup = (1, 2, 3, 4, 5)
@@ -114,3 +113,74 @@ tup = (1, 2, 3, 4, 5)
 print(tup)
 print('--------------------------------------')
 
+# 문자열 == 문자 리스트
+title = 'python'
+print(title[0])
+# TypeError: 'str' object does not support item assignment
+# title[0]= 'p'
+# 문자열에서는 값 변경 x
+print('P' + title[1:])
+
+# 일반적인 문자형 리스트
+text = ['p', 'y', 't', 'h', 'o', 'n']
+print(text)
+text[0] = 'P'
+print(text)
+print('--------------------------------------')
+
+# 문자열 포맷팅 - 문자열을 내 마음대로 바꿔 쓰는것
+# 문자열에서 {}는 연산자 역할
+print("I'm so happy {0} you!!, {1}".format(2, 'Hey'))
+# 최신식 문자열 포맷팅
+# 맨 앞에 f 빠지면 큰일남
+preword = 3
+sayHello = 'Hey'
+print(f"I'm so happy {preword} you!!, {sayHello}")
+print('--------------------------------------')
+
+# 주의 사항 - 소수점 표시하는 법
+pi = 3.141592
+print(f'파이는 {pi}입니다.')
+# 소수점 2째 자리까지 자르기
+print(f'파이는 {pi:0.2f}입니다.')
+print(f'파이는 {pi:10.3f}입니다.')
+print('--------------------------------------')
+
+# 문자열 특정 문자 기준으로 자르기
+full_name = 'Park Seong Hyeon'
+vals = full_name.split()    # 스페이스
+print(type(vals))
+print(vals)
+
+vals = full_name.split('S') # 지정
+print(vals)
+
+# 특정 문자열 바꾸기
+print(full_name.replace('Park', 'Ashely'))
+
+# 문자열 공백 제거
+hi = '          Hello~ Bye          '
+print(hi.lstrip() + '|')    # 왼쪽 공백 삭제, | 파이프
+print(hi.rstrip() + '|')    # 오른쪽 공백 삭제
+print(hi.strip() + '|')     # 양쪽 공백 삭제
+
+# 문자열에서 값 찾기 index, find - 위치 찾아줌
+print(full_name.index('S'))
+# print(full_name.index('z')) 
+# ValueError: substring not found
+
+print(full_name.find('e'))  
+print(full_name.find('z'))  # 결과 값이 마이너스 값이면 찾는 값이 없다는 소리
+
+# count - 찾는 단어의 개수를 알려줌
+print(full_name.count('e'))
+
+# 모든 단어를 대문자 또는 소문자로 변경
+print(full_name.upper())    # 대문자
+print(full_name.lower())    # 소문자
+
+print('--------------------------------------')
+
+# 연산자 우선순위
+print(3 + 4 * 2)
+print((3 + 4) * 2)
